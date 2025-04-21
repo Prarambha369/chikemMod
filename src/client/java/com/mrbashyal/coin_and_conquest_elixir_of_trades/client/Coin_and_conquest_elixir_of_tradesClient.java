d33d846ff4cb3bd65dbd6db6677f9015a81ab170
mrbashyal <prarambhabashyal@gmail.com>
2025-04-22 03:40:54 +0545

    Initial commit

---
+++ src/client/java/com/mrbashyal/coin_and_conquest_elixir_of_trades/client/Coin_and_conquest_elixir_of_tradesClient.java
@@ -0,0 +1,10 @@
+package com.mrbashyal.coin_and_conquest_elixir_of_trades.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Coin_and_conquest_elixir_of_tradesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
