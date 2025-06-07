86e8bda25b1c4589f11d6ebf8e04c6e1da748a25
mrbashyal <prarambhabashyal@gmail.com>
2025-06-07 19:34:08 +0545

    fix: remove extraneous character in register method of ModEntityAttributes

--- src/main/java/mr/bashyal/chickenmod/registry/ModEntityAttributes.java
+++ src/main/java/mr/bashyal/chickenmod/registry/ModEntityAttributes.java
@@ -4,7 +4,7 @@ import mr.bashyal.chickenmod.entity.MountableChickenEntity;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 
 public class ModEntityAttributes {
-    public static void register() {
+    public static void register() {x
         FabricDefaultAttributeRegistry.register(ModEntities.MOUNTABLE_CHICKEN, MountableChickenEntity.createAttributes());
     }
 }
