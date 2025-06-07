73121a801622882ac348ab159e555ece0a05c841
mrbashyal <prarambhabashyal@gmail.com>
2025-06-07 21:54:47 +0545

    Changes by akaalroopsingh

--- src/main/java/mr/bashyal/chickenmod/ChickenMod.java
+++ src/main/java/mr/bashyal/chickenmod/ChickenMod.java
@@ -2,14 +2,6 @@ package mr.bashyal.chickenmod;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import com.mojang.brigadier.CommandDispatcher;
-import net.minecraft.server.command.ServerCommandSource;
-import mr.bashyal.chickenmod.registry.ModEntities;
-import mr.bashyal.chickenmod.entity.MountableChickenEntity;
-
-import static net.minecraft.server.command.CommandManager.literal;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.text.Text;
 
 public class ChickenMod implements ModInitializer {
     @Override
